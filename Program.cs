4f5696433409c75cfe9f16c1104671dbabb63b3c
mandara11 <mandaram2345@gmail.com>
2022-07-28 14:34:27 +0530

    Added: UC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWage Program");
+            EmployeeCheck.CheckEmployeeAttendence();
         }
     }
 }
