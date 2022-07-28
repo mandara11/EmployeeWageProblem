5529a16f9c8e895f721269e23b94a9b59c9c33ce
mandara11 <mandaram2345@gmail.com>
2022-07-28 15:05:08 +0530

    Calculate Empployeewage

---
+++ DailyEmpWage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    //UC2
+    public static class DailyEmpWage
+    {
+        public static void CheckEmployeeAttendence()
+        {
+            //constants
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random obj = new Random();
+            int empCheck = obj.Next(2);
+            if (empCheck==IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs=0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage : " + empWage);
+        }
+    }
+}
