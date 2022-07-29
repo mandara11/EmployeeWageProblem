2c3c0fc043ffbe571ca88aa8f4ad503cb1aef4c3
mandara11 <mandaram2345@gmail.com>
2022-07-29 18:38:56 +0530

    Added:UC6

--- Program.cs
+++ Program.cs
@@ -5,10 +5,12 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWage Program");
-            EmployeeCheck.CheckEmployeeAttendence(); //UC1
-            DailyEmpWage.CheckEmployeeAttendence();  //UC2
-            PartTime.AddPartTime();  //UC3
-            PartTimeSwitch.AddPartTimeSwitch(); //UC4
+            //EmployeeCheck.CheckEmployeeAttendence(); //UC1
+            //DailyEmpWage.CheckEmployeeAttendence();  //UC2
+            //PartTime.AddPartTime();  //UC3
+            //PartTimeSwitch.AddPartTimeSwitch(); //UC4
+            //MonthWages.WagesforMonth(); //UC5
+            CalculateMonthlyWage.CalculateMonthlyEmpWage();
         }
     }
 }
